ab91a95f3866df140f8071d2d1c35e4fb078de1a
Alex Lenson <144947695+AlexLenson@users.noreply.github.com>
2023-11-08 15:30:02 -0500

    added modal with data from table rows that user clicks

---
+++ public/js/modal.js
@@ -0,0 +1,60 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const tableRows = document.querySelectorAll(".table tbody tr");
+    const modal = document.querySelector(".modal");
+    const modalBackground = document.querySelector(".modal-background");
+    const modalPetName = modal.querySelector("#pet-name-modal");
+    const modalPhoto = modal.querySelector("#pet-picture-modal");
+    const modalPetBreed = modal.querySelector("#pet-breed-modal");
+    const modalPetAge = modal.querySelector("#pet-age-modal");
+    const modalPetSex = modal.querySelector("#pet-sex-modal");
+    const modalPetColor = modal.querySelector("#pet-color-modal");
+    const modalPetSize = modal.querySelector("#pet-size-modal");
+    const modalIntakeDate = modal.querySelector("#pet-intake-date-modal");
+    const cancelButton = modal.querySelector("#cancelButton");
+    const closeButton = modal.querySelector(".delete");
+
+    // Add a click event listener to each table row
+    tableRows.forEach((row) => {
+        row.addEventListener("click", function () {
+            // Get the data from the clicked row
+            const rowData = {
+                petName: row.cells[2].textContent,
+                photo: row.cells[9].textContent, // Update with appropriate image URL
+                petBreed: row.cells[6].textContent,
+                petAge: row.cells[3].textContent,
+                petSex: row.cells[7].textContent,
+                petColor: row.cells[5].textContent,
+                petSize: row.cells[4].textContent,
+                intakeDate: row.cells[1].textContent,
+            };
+
+            // Populate the modal with the row data
+            modalPetName.textContent = rowData.petName;
+            modalPhoto.src = rowData.photo;
+            modalPetBreed.textContent = rowData.petBreed;
+            modalPetAge.textContent = rowData.petAge;
+            modalPetSex.textContent = rowData.petSex;
+            modalPetColor.textContent = rowData.petColor;
+            modalPetSize.textContent = rowData.petSize;
+            modalIntakeDate.textContent = rowData.intakeDate;
+
+            // Display the modal when a row is clicked
+            modal.classList.add("is-active");
+        });
+    });
+
+    // Close the modal when the background is clicked
+    modalBackground.addEventListener("click", function () {
+        modal.classList.remove("is-active");
+    });
+
+    // Close the modal when the "cancel" button is clicked
+    cancelButton.addEventListener("click", function () {
+        modal.classList.remove("is-active");
+    });
+
+    // Close the modal when the close button is clicked
+    closeButton.addEventListener("click", function () {
+        modal.classList.remove("is-active");
+    });
+});
